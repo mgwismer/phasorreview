ab737bca41fbc579ed8daa8e5fd37c1bbf28eb35
Margaret Wismer <margaretgwismer@gmail.com>
2020-04-14 12:41:43 -0400

    Updates

---
+++ precache-manifest.f54121fc29236fea407ddc9b736ab726.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d25dde866c3c312c69821540b57c20a",
+    "url": "/phasorreview/index.html"
+  },
+  {
+    "revision": "e5706edbccf21e41762e",
+    "url": "/phasorreview/static/css/main.2ef894fc.chunk.css"
+  },
+  {
+    "revision": "9512d12f42e59e9cb1e4",
+    "url": "/phasorreview/static/js/2.773b3905.chunk.js"
+  },
+  {
+    "revision": "81fbb27446145c382bc1dab229a0d054",
+    "url": "/phasorreview/static/js/2.773b3905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5706edbccf21e41762e",
+    "url": "/phasorreview/static/js/main.42810b7f.chunk.js"
+  },
+  {
+    "revision": "72b3c34a0a22b0646bec",
+    "url": "/phasorreview/static/js/runtime-main.9913cadd.js"
+  },
+  {
+    "revision": "e1e83a14b2bb82f3307c98b5c08e3e10",
+    "url": "/phasorreview/static/media/euler_portrait.e1e83a14.gif"
+  },
+  {
+    "revision": "231ab30e516ca2437334f0f24419ea2a",
+    "url": "/phasorreview/static/media/power6-3.231ab30e.gif"
+  },
+  {
+    "revision": "b5f776a95b3efdc9c90fb58701a09f5d",
+    "url": "/phasorreview/static/media/power7-1.b5f776a9.gif"
+  },
+  {
+    "revision": "37522037226f92dd97ea53040c50b2f0",
+    "url": "/phasorreview/static/media/review4-1.37522037.gif"
+  },
+  {
+    "revision": "508736bf4a419b7cce26e40b534e28d7",
+    "url": "/phasorreview/static/media/z.508736bf.gif"
+  }
+]);
